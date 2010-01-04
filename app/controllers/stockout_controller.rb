5ca413454f11c1f98ad545c04d58d69409a2edca
Atsuhiko Kimura <a-kimura@beacon-it.co.jp>
2010-01-04 18:16:34 +0900

    fixed: can't create and download pdf files on war

--- app/controllers/stockout_controller.rb
+++ app/controllers/stockout_controller.rb
@@ -19,7 +19,7 @@ class StockoutController < ApplicationController
       csv = value.to_csv([:product_name, :purchase_quantity, :price])
       template_path = File.join(RAILS_ROOT, 'config/pdf', 'naiji.jasper')
       pdf_name = supplier.id.to_s + '_' + format_month(params) + '_' + Time.now.strftime('%Y%m%d') + '.pdf'
-      pdf_path = File.join(RAILS_ROOT, 'public','pdf')
+      pdf_path = File.join(Rails.public_path,'pdf')
       pdf_path += File::SEPARATOR + pdf_name
       if !File.exist?(pdf_path)
         JasperReport.build_pre_order(csv, supplier, template_path, pdf_path)
